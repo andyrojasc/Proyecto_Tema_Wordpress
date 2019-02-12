18e795f3e912dac748590e4d688eeeb4a1537462
justinperezd <43105524+justinperezd@users.noreply.github.com>
2019-02-11 20:16:48 -0600

    Avance
    perfil de atleta

---
+++ page-perfil.php
@@ -0,0 +1,30 @@
+<?php get_header(); ?>
+    <div class="container-fluid">
+        <div class="row alignVertical">
+            <div class="col-12 bannerContactenos text-center" class="d-block w-100" style="background-image: url(<?php echo bloginfo('template_directory'); ?>/assets/img/perfil/tencio.png);">
+                <div class="alignVerticalContacts texto-foto">
+                    <h1 class="h_fonts text-center textShadow text-white">Kenneth Tencio Pollis</h1>
+                    <h4 class="h_fonts nombre-atleta">Costa Rica</h4>
+                    <p class="texto-citado h_fonts">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Commodi dolor aperiam harum ea voluptatum enim.</p>
+                    <h2 class="h_fonts deporte">BMX</h2>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="container datos">
+        <div class="row mt-4 pt-4">
+            <div class="col-12 col-lg-4 text-white borde pt-2">
+                <h4>Nacimiento</h4>
+                <h5>00/00/00</h5>
+            </div>
+            <div class="col-12 col-lg-4 text-white borde pt-2">
+                <h4>Inicio carrera</h4>
+                <h5>00/00/00</h5>
+            </div>
+            <div class="col-12 col-lg-4 text-white  pt-2">
+                <h4>Nacionalidad</h4>
+                <h5>Costarricense</h5>
+            </div>
+        </div>
+    </div>
+<?php get_footer(); ?>
