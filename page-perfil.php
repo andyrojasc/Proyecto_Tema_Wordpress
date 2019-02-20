64e90b984baba627a0f009678d4a1977f982e880
justinperezd <jeaustinperezd@gmail.com>
2019-02-20 02:47:12 -0600

    CMS FINAL

--- page-perfil.php
+++ page-perfil.php
@@ -2,12 +2,12 @@
 <div class="body-perfil">
     <div class="container-fluid">
         <div class="row alignVertical">
-            <div class="col-12 bannerContactenos text-center" class="d-block w-100" style="background-image: url(<?php echo bloginfo('template_directory'); ?>/assets/img/perfil/tencio.png);">
+        <div class="col-12 bannerContactenos text-center" style="<?php if ( get_field( 'main-img-deportista') ) { ?>  background-image: url(<?php the_field( 'main-img-deportista' ); ?>);  <?php } ?>">
                 <div class="alignVerticalContacts texto-foto">
-                    <h1 class="h_fonts text-center textShadow text-white">Kenneth Tencio Pollis</h1>
-                    <h4 class="h_fonts nombre-atleta">Costa Rica</h4>
-                    <p class="texto-citado h_fonts">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Commodi dolor aperiam harum ea voluptatum enim.</p>
-                    <h2 class="h_fonts deporte">BMX</h2>
+                    <h1 class="h_fonts text-center textShadow text-white"><?php the_field( 'nombre-deportista' ); ?></h1>
+                    <h4 class="h_fonts nombre-atleta"><?php the_field( 'pais-deportista' ); ?></h4>
+                    <p class="texto-citado h_fonts"><?php the_field( 'frase-deportista' ); ?></p>
+                    <h2 class="h_fonts deporte"><?php the_field( 'deporte' ); ?></h2>
                 </div>
             </div>
         </div>
@@ -15,16 +15,16 @@
     <div class="container">
         <div class="row mt-3 pt-4">
             <div class="col-12 col-lg-4 text-white borde pt-2">
-                <h4>Nacimiento</h4>
-                <h5>00/00/00</h5>
+                <h4><?php the_field( 'titulo-detalle-1' ); ?></h4>
+                <h5><?php the_field( 'detalle-deportista-1' ); ?></h5>
             </div>
             <div class="col-12 col-lg-4 text-white borde pt-2">
-                <h4>Inicio carrera</h4>
-                <h5>00/00/00</h5>
+                <h4><?php the_field( 'titulo-detalle-2' ); ?></h4>
+                <h5><?php the_field( 'detalle-deportista-2' ); ?></h5>
             </div>
             <div class="col-12 mb-4 col-lg-4 text-white  pt-2">
-                <h4>Nacionalidad</h4>
-                <h5>Costarricense</h5>
+                <h4><?php the_field( 'titulo-detalle-3' ); ?></h4>
+                <h5><?php the_field( 'detalle-deportista-3' ); ?></h5>
             </div>
         </div>
     </div>
@@ -32,16 +32,8 @@
     <div class="container mt-3 mb-3 ">
         <div class="row">
             <div class="col ">
-            <h2 class="text-center h_fonts">Historia personal y carrera profesional</h2>
-                <p class="text-justify texto-descriptivo p-3">Lorem ipsum dolor, sit amet consectetur adipisicing elit. 
-                    Voluptas magnam nisi placeat suscipit ad aliquid 
-                    repellat quibusdam id consequatur illum adipisci 
-                    commodi accusamus animi, veritatis architecto possimus 
-                    aspernatur neque quasi nulla perferendis. Odio neque 
-                    xpedita corporis repellendus sint dignissimos molestiae 
-                    dolorum magni qui, et at, 
-                    doloribus consectetur quos laboriosam corrupti!
-                </p>
+            <h2 class="text-center h_fonts"><?php the_field( 'titulo-biografia' ); ?></h2>
+                <p class="text-justify texto-descriptivo p-3"><?php the_field( 'biografia-texto' ); ?></p>
             </div>
         </div>
     </div>
